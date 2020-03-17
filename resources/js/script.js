487000d60961d6e1f4c3cc3b9da3a3f4991a181e
DraganUS <usic.d.dragan@gmail.com>
2020-03-17 17:43:10 +0100

    Second commit

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video');
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('/js/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/js/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/js/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/js/models')
 ]).then(startVideo);
 
 function startVideo() {
---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                        <video id="video" width="720" height="560" autoplay muted></video>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
