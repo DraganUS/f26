836d564d30e7c413ba61ffb6c6b65969dad1e7d3
DraganUS <usic.d.dragan@gmail.com>
2020-04-03 22:28:19 +0200

    adding image site do admin

--- resources/js/components/ShowImage.vue
+++ resources/js/components/ShowImage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
+        <div style="display: flex; justify-content: center;" class="row justify-content-center">
             <div class="col-md-8" >
                 <h3>Testing instructions:</h3>
                 <div>
