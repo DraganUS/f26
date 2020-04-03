3fc49adfe741efab4b26b0c10f297e69b31d8820
DraganUS <usic.d.dragan@gmail.com>
2020-04-03 19:31:35 +0200

    adding image site do admin

--- resources/views/voyager/gravy.blade.php
+++ resources/views/voyager/gravy.blade.php
@@ -2,15 +2,10 @@
 @section('content')
     <div class="container">
         <h2>ads</h2>
-        <div class="row justify-content-center">
-            <div class="mx-3 "style="z-index: 1">
-                <home-main-component></home-main-component>
                 <home-2-component></home-2-component>
-                <home-3-component></home-3-component>
-                <how-it-works-component></how-it-works-component>
-            </div>
 {{--            <video id="video" width="720" height="560" autoplay muted></video>--}}
 {{--            <show-image-component products-db="{{json_encode($products)}}"></show-image-component>--}}
-        </div>
+
     </div>
 @stop
+<home-2-component></home-2-component>
