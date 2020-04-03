0624903cfee00da69c2ae4021ca34878cbf43f93
DraganUS <usic.d.dragan@gmail.com>
2020-04-04 00:02:48 +0200

    adding image site do admin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -139,7 +139,8 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item mr-3">
-{{--                                <a class="nav-link" href="{{ route('/login') }}">{{ __('Login') }}</a>--}}
+{{--                                <a class="nav-link" href="{{ route('/admin/login') }}">Login</a>--}}
+                                <a class="nav-link" href="/admin/login">Login</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item" id="register">
