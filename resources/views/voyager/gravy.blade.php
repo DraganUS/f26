c323c8d83d1054a2dce88e987bad7817420068f9
DraganUS <usic.d.dragan@gmail.com>
2020-04-03 20:12:05 +0200

    adding image site do admin

--- resources/views/voyager/gravy.blade.php
+++ resources/views/voyager/gravy.blade.php
@@ -2,8 +2,10 @@
 @section('content')
     <div id="app" class="container">
         <h2>A1</h2>
+
                 <home-2-component></home-2-component>
 {{--            <video id="video" width="720" height="560" autoplay muted></video>--}}
 {{--            <show-image-component products-db="{{json_encode($products)}}"></show-image-component>--}}
     </div>
+    <script src="{{ asset('js/app.js') }}"></script>
 @stop
