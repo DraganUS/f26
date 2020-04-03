588d38436fa4cb0481bf23cb42234e0ad248f02b
DraganUS <usic.d.dragan@gmail.com>
2020-04-03 18:12:14 +0200

    adding image site do admin

--- resources/views/voyager/gravy.blade.php
+++ resources/views/voyager/gravy.blade.php
@@ -3,7 +3,9 @@
     <div class="container">
         <div class="row justify-content-center">
             <video id="video" width="720" height="560" autoplay muted></video>
+
 {{--            <show-image-component products-db="{{json_encode($products)}}"></show-image-component>--}}
+
         </div>
     </div>
 @stop
