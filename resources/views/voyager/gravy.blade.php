436b1899d36ee748641c219f7483c2614ce947fb
DraganUS <usic.d.dragan@gmail.com>
2020-04-03 18:01:37 +0200

    adding image site do admin

--- resources/views/voyager/gravy.blade.php
+++ resources/views/voyager/gravy.blade.php
@@ -1,4 +1,10 @@
 @extends('voyager::master')
 @section('content')
-    <p>Biscuits NEED Gravy!</p>
+    <div class="container">
+        <div class="row justify-content-center">
+            <video id="video" width="720" height="560" autoplay muted></video>
+            <show-image-component products-db="{{json_encode($products)}}"></show-image-component>
+            {{--        <video id="video" width="720" height="560" autoplay muted></video>--}}
+        </div>
+    </div>
 @stop
