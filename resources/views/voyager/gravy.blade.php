cde2efceb66fcc7c4abce0a14c32fa5afac57e26
DraganUS <usic.d.dragan@gmail.com>
2020-04-05 20:29:08 +0200

    adding facial

--- resources/views/voyager/gravy.blade.php
+++ resources/views/voyager/gravy.blade.php
@@ -2,6 +2,7 @@
 @section('content')
     <div id="app" class="container">
 {{--            <video id="video" width="720" height="560" autoplay muted></video>--}}
+        <script defer src="/js/face-api.min.js"></script>
         <show-image-component products-db="{{json_encode($products)}}"></show-image-component>
     </div>
     <script src="{{ asset('js/app.js') }}"></script>
