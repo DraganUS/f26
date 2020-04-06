754de38f159f617f229362e6c4769500b1388add
DraganUS <usic.d.dragan@gmail.com>
2020-04-06 15:06:49 +0200

    face

--- resources/views/voyager/gravy.blade.php
+++ resources/views/voyager/gravy.blade.php
@@ -1,10 +1,17 @@
 @extends('voyager::master')
 @section('content')
+    <style>
+        canvas {
+            position: absolute;
+            top: 68px;
+            left: 244px;
+        }
+    </style>
     <script defer src="/js/face-api.min.js"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script defer src="/js/script.js"></script>
     <div id="app" class="container">
             <show-image-component products-db="{{json_encode($products)}}"></show-image-component>
     </div>
+    <script src="{{ asset('js/app.js') }}"></script>
     <video id="video" width="720" height="560" autoplay muted></video>
-    <script defer src="/js/script.js"></script>
 @stop
