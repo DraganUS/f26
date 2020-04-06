468aead7e6a9ffcb73b7e6de337d9c7637a106f1
DraganUS <usic.d.dragan@gmail.com>
2020-04-06 18:32:25 +0200

    face

--- resources/views/voyager/gravy.blade.php
+++ resources/views/voyager/gravy.blade.php
@@ -8,11 +8,11 @@
         }
     </style>
     <script defer src="/js/face-api.min.js"></script>
-    <video id="video" width="320" height="260" autoplay muted></video>
-    <script defer src="/js/script.js"></script>
     <div id="app" class="container">
+            <video id="video" width="320" height="260" autoplay muted></video>
             <show-image-component products-db="{{json_encode($products)}}"></show-image-component>
     </div>
+    <script defer src="/js/script.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
 @stop
 
