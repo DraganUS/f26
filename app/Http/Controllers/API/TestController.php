e6fb27b33098497eadd5d1efc7a895acddf9e88e
DraganUS <usic.d.dragan@gmail.com>
2020-04-18 19:58:15 +0200

    api test controller

---
+++ app/Http/Controllers/API/TestController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Test;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TestController extends Controller
+{
+
+    public function store(Request $request)
+    {
+//        $userId = Auth::user()->id;
+        $userId = 1;
+        $newTest = new Test();
+        $newTest->user_id = $userId;
+        $newTest->product_id = $request->product_id;
+        $newTest->facial_expresion = $request->facial_expresion;
+        $newTest->save();
+
+        return responder()->success($request)->respond();
+
+
+    }
+
+    public function destroy($id)
+    {
+
+    }
+}
