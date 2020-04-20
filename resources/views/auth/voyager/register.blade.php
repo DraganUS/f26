1c934a8ccb1988408b05d8e0f9ba816b60892a18
DraganUS <usic.d.dragan@gmail.com>
2020-04-20 15:14:19 +0200

    auto upload

--- resources/views/auth/voyager/register.blade.php
+++ resources/views/auth/voyager/register.blade.php
@@ -70,14 +70,15 @@
                 <form method="POST" action="{{ route('register') }}">
                     @csrf
 
-                    <div class="form-group row form-group-default" id="emailGroup">
+                    <div class="form-group row form-group-default" >
                         <label for="name">{{ __('Name') }} </label>
                         <div class="controls">
                             <input
                                 id="name"
                                 type="text"
                                 class="form-control @error('name')
-                                    is-invalid @enderror" name="name"
+                                    is-invalid @enderror"
+                                name="name"
                                 value="{{ old('name') }}"
                                 required
                                 autocomplete="name"
@@ -102,12 +103,33 @@
                                 id="email"
                                 value="{{ old('email') }}"
                                 placeholder="{{ __('voyager::generic.email') }}"
-                                class="form-control @error('email') is-invalid @enderror"
+                                class="form-control @error('name') is-invalid @enderror"
                                 autocomplete="email"
                                 required
                             >
-                            @error('email')
+                            @error('name')
                                     <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group form-group-default" >
+                        <label>Name</label>
+                        <div class="controls">
+                            <input
+                                type="text"
+                                name="text"
+                                id="text"
+                                value="{{ old('name') }}"
+                                placeholder="{{ __('voyager::generic.email') }}"
+                                class="form-control @error('email') is-invalid @enderror"
+                                autocomplete="name"
+                                required
+                            >
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                             @enderror
