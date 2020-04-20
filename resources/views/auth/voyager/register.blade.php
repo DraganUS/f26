30fc6ab3eddf90cf0c2c4535b1e005780ed4986d
DraganUS <usic.d.dragan@gmail.com>
2020-04-20 14:56:03 +0200

    auto upload

--- resources/views/auth/voyager/register.blade.php
+++ resources/views/auth/voyager/register.blade.php
@@ -84,7 +84,7 @@
                         </div>
                     </div>
 
-                    <div class="form-group row">
+                    <div class="form-group form-group-default" id="emailGroup">
                         <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                         <div class="col-md-6">
