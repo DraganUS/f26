87cd7de6aeea21d15c5210fb7ae25ca54e4bdb4f
DraganUS <usic.d.dragan@gmail.com>
2020-04-20 02:36:48 +0200

    auto upload

--- app/Http/Controllers/ProductTestingController.php
+++ app/Http/Controllers/ProductTestingController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class ProductTestingController extends Controller
 {
     public function index()
     {
         $products = Product::all();
-        $id = auth()-id;
+        $id = Auth::user()->id;
         return view('/voyager/gravy', compact('products', 'id'));
     }
 }
