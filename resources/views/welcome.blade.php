4357c5d0edffcc3c25b74d9e1690d3352ad44861
DraganUS <usic.d.dragan@gmail.com>
2020-04-20 14:19:48 +0200

    auto upload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -154,6 +154,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="/admin">
+                                        Dashboard
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -162,11 +165,6 @@
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
-
-                                    <a class="dropdown-item" href="{{ route('logout') }}">
-                                        Dashboard
-                                    </a>
-
                                 </div>
                             </li>
                         @endguest
