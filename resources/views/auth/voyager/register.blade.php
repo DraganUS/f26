5d24cbdfcf3a8a98c37cc7b6d116bdfd1c1013e6
DraganUS <usic.d.dragan@gmail.com>
2020-04-20 15:05:20 +0200

    auto upload

--- resources/views/auth/voyager/register.blade.php
+++ resources/views/auth/voyager/register.blade.php
@@ -70,43 +70,47 @@
                 <form method="POST" action="{{ route('register') }}">
                     @csrf
 
-                    <div class="form-group row">
-                        <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                    <div class="form-group row form-group-default">
+                        <label
+                            for="name"
+                            class="col-md-4 col-form-label text-md-right"
+                        >
+                            {{ __('Name') }}
+                        </label>
 
                         <div class="col-md-6">
-                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                            <input
+                                id="name"
+                                type="text"
+                                class="form-control @error('name')
+                                    is-invalid @enderror" name="name"
+                                value="{{ old('name') }}"
+                                required
+                                autocomplete="name"
+                                autofocus
+                            >
 
                             @error('name')
                             <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <strong>{{ $message }}</strong>
+                            </span>
                             @enderror
                         </div>
                     </div>
 
-{{--                    <div class="form-group form-group-default" id="emailGroup">--}}
-{{--                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>--}}
-
-{{--                        <div class="col-md-6">--}}
-{{--                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">--}}
-
-{{--                            @error('email')--}}
-{{--                            <span class="invalid-feedback" role="alert">--}}
-{{--                                        <strong>{{ $message }}</strong>--}}
-{{--                                    </span>--}}
-{{--                            @enderror--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
 
                     <div class="form-group form-group-default" id="emailGroup">
                         <label>{{ __('voyager::generic.email') }}</label>
                         <div class="controls">
-                            <input type="email" name="email"
-                                   id="email" value="{{ old('email') }}"
-                                   placeholder="{{ __('voyager::generic.email') }}"
-                                   class="form-control @error('email') is-invalid @enderror"
-                                   autocomplete="email"
-                                   required
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                value="{{ old('email') }}"
+                                placeholder="{{ __('voyager::generic.email') }}"
+                                class="form-control @error('email') is-invalid @enderror"
+                                autocomplete="email"
+                                required
                             >
                             @error('email')
                                     <span class="invalid-feedback" role="alert">
