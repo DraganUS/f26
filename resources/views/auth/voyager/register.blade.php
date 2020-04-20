da189384b665c14d0b7e7e40f0291aec833b5053
DraganUS <usic.d.dragan@gmail.com>
2020-04-20 15:27:16 +0200

    auto upload

--- resources/views/auth/voyager/register.blade.php
+++ resources/views/auth/voyager/register.blade.php
@@ -114,12 +114,18 @@
                         </div>
                     </div>
 
-                    <div class="form-group row">
-                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                        <div class="col-md-6">
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                    <div class="form-group form-group-default">
+                        <label>{{ __('Password') }}</label>
+                        <div class="controls">
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                class="form-control @error('password') is-invalid @enderror"
+                                autocomplete="new-password"
+                                required
+                            >
                             @error('password')
                             <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -128,13 +134,22 @@
                         </div>
                     </div>
 
-                    <div class="form-group row">
-                        <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                        <div class="col-md-6">
-                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                    <div class="form-group form-group-default">
+                        <label>{{ __('Confirm Password') }}</label>
+                        <div class="controls">
+                            <input
+                                type="password"
+                                name="password_confirmation"
+                                id="password-confirm"
+                                class="form-control"
+                                autocomplete="new-password"
+                                required
+                            >
                         </div>
                     </div>
+
+
+
 
                     <div class="form-group row mb-0">
                         <div class="col-md-6 offset-md-4">
