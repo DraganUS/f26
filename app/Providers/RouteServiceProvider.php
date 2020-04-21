76a6c48ef3c72f516add8032b76eb7c20010da13
DraganUS <usic.d.dragan@gmail.com>
2020-04-22 01:25:45 +0200

    auto upload

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/voyager/gravy';
+    public const HOME = '/voyager/info';
 
     /**
      * Define your route model bindings, pattern filters, etc.
