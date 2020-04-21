fad8414e3a7e13ff71d5a445eee06f9fb6c30257
DraganUS <usic.d.dragan@gmail.com>
2020-04-22 01:14:56 +0200

    auto upload

--- resources/views/voyager/info.blade.php
+++ resources/views/voyager/info.blade.php
@@ -2,13 +2,28 @@
 @section('content')
     <style></style>
     <div class="container">
+        <div class="mb-7">
+            <h2>Testing instructions:</h2>
+            <div class="mb-7">
+                <h3>Prerequisites:</h3>
+                1. <span style="background: #FFC107">! NOTIFICATION !</span> This test will be using your web camera.<br>
+                2. Your Laptop or Computer should have web camera installed and ready for use.<br>
+                3. For the duration of the test, please position yourself (your face) in the center of the camera window.<br>
+                <h3>Test procedure:</h3>
+                1. Look strait at the screen<br>
+                2. On the “Test Window” press play button once and wait for a picture to show up.<br>
+                3. Picture will be displayed for a very short time. This is normal, and actually the point of this test.<br>
+                4. After you see the picture, press play again.<br>
+                5. Test procedure is over when you go through this process 5 times (see 5 pictures).<br>
+                <h3>Thank you in advance!</h3>
+                <h4 style="color: red" v-if="counter">remaining tests: {{this.products.length}}</h4>
+            </div>
+        </div>
         <div>
-            <h3>Prerequisites:</h3>
-            1. <span style="background: #FFC107">! NOTIFICATION !</span> This test will be using your web camera.<br>
-            2. Your Laptop or Computer should have web camera installed and ready for use.<br>
-            3. For the duration of the test, please position yourself (your face) in the center of the camera window.<br>
+
         </div>
         <img style="max-height: 500px;" src="https://tahi26.de//storage/Screenshot%202020-04-22%20at%2000.54.44.png" alt="">
+
     </div>
 @stop
 
