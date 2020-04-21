24b25adff5f4a58922327ec1ba405d56e60a8a12
DraganUS <usic.d.dragan@gmail.com>
2020-04-22 01:06:49 +0200

    auto upload

--- resources/views/voyager/info.blade.php
+++ resources/views/voyager/info.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <style></style>
     <div class="container">
-        <img src="https://tahi26.de//storage/Screenshot%202020-04-22%20at%2000.54.44.png" alt="">
+        <img style="    max-height: 500px;" src="https://tahi26.de//storage/Screenshot%202020-04-22%20at%2000.54.44.png" alt="">
     </div>
 @stop
 
