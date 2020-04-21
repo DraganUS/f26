901ea1043ac49a215b1353429096f4a9e63afe12
DraganUS <usic.d.dragan@gmail.com>
2020-04-22 01:10:47 +0200

    auto upload

--- resources/views/voyager/info.blade.php
+++ resources/views/voyager/info.blade.php
@@ -2,7 +2,13 @@
 @section('content')
     <style></style>
     <div class="container">
-        <img style="    max-height: 500px;" src="https://tahi26.de//storage/Screenshot%202020-04-22%20at%2000.54.44.png" alt="">
+        <div>
+            <h3>Prerequisites:</h3>
+            1. <span style="background: #FFC107">! NOTIFICATION !</span> This test will be using your web camera.<br>
+            2. Your Laptop or Computer should have web camera installed and ready for use.<br>
+            3. For the duration of the test, please position yourself (your face) in the center of the camera window.<br>
+        </div>
+        <img style="max-height: 500px;" src="https://tahi26.de//storage/Screenshot%202020-04-22%20at%2000.54.44.png" alt="">
     </div>
 @stop
 
